568dc05e3f7735c688d4433c1361d29f5246b335
Gustavo Acco Sendeske <gustavo.sendeske@gmail.com>
2023-04-22 18:16:17 -0300

    cleanup shader file

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -32,7 +32,6 @@ ShaderProgramSource Shader::ParseShader(const std::string& filepath)
     std::string line;
     std::stringstream ss[2];
 
-    std::cout << filepath << std::endl;
     ShaderType type = ShaderType::NONE;
     while (getline(stream, line))
     {
@@ -45,7 +44,6 @@ ShaderProgramSource Shader::ParseShader(const std::string& filepath)
         }
         else 
         {
-            std::cout << "type: " << (int)type << "\n" << line << std::endl;
             ss[(int)type] << line << "\n";
         }
     }
@@ -55,25 +53,24 @@ ShaderProgramSource Shader::ParseShader(const std::string& filepath)
 
 unsigned int Shader::CompileShader(unsigned int type, const std::string& source)
 {
-
-    unsigned int id = glCreateShader(type);
+    GLCall(unsigned int id = glCreateShader(type));
     const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+    GLCall(glShaderSource(id, 1, &src, nullptr));
+    GLCall(glCompileShader(id));
 
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
     if (result == GL_FALSE)
     {
         int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
         char *message = (char*) alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
+        GLCall(glGetShaderInfoLog(id, length, &length, message));
 
         std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!"<< std::endl;
         std::cout << message << std::endl;
 
-        glDeleteShader(id);
+        GLCall(glDeleteShader(id));
         return 0;
     }
 
