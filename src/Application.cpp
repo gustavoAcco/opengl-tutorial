c4f79a0ed33fad391e835a505c23015ccf7c6a67
Gustavo Acco Sendeske <gustavo.sendeske@gmail.com>
2023-04-24 20:54:09 -0300

    add texture test

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,7 +3,6 @@
 
 #include "Renderer.h"
 
-#include "Test.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
@@ -19,6 +18,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -84,6 +84,7 @@ int main(void)
         currentTest = testMenu;
         
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
         while (!glfwWindowShouldClose(window))
         {
